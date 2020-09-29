1b06530390ad78c07041b0521ed40af53db1c4bd
Magdalena Schuetz <m.schuetz@outlook.de>
2020-09-29 03:50:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     var count = [];
     for (let i = 0; i < katzDeliLine.length; i++) {
-    count.push(`${[i+1]}. ${katzDeliLine[i]}`);
+    count.push(`${[i+1]}. ${katzDeliLine[i]} `);
     }
-    return `The line is currently: ${count} `;
+    return `The line is currently: ${count}`;
   }
   else if (katzDeliLine.length === 0) {
     return `The line is currently empty.`;
