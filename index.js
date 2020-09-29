ed1ace8ba93d394b83542ceefb5d88d94e51814a
Magdalena Schuetz <m.schuetz@outlook.de>
2020-09-29 03:16:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function takeANumber(katzDeli,name) {
+  katzDeli.push(`${name}`);
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+}
+
+function nowServing(katzDeliLine){
+  for (let i = 0; i < katzDeliLine.length; i++)
+    if (katzDeliLine.length > 0) {
+      var serving = `Currently serving ${katzDeliLine[0]}.`;
+      katzDeliLine.slice(1);
+      return serving;
+    }
+    else if (katzDeliLine.length === 0) {
+      var served = `There is nobody waiting to be served!`;
+      return served;
+    }
+}
+
